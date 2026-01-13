ff840ec867769f6e0995c1df506277dd737ab1d2
Huidae Cho <grass4u@gmail.com>
2026-01-13 00:39:41 -0700

    Run g.mkfontcap instead of skipping it

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,33 +1,7 @@
 #!/bin/sh
 set -e
 
-echo "===== conda-build env ====="
-echo "CONDA_BUILD_CROSS_COMPILATION=${CONDA_BUILD_CROSS_COMPILATION}"
-echo "target_platform=${target_platform}"
-echo "BUILD=${BUILD}"
-echo "HOST=${HOST}"
-echo "PREFIX=${PREFIX}"
-echo "BUILD_PREFIX=${BUILD_PREFIX}"
-echo "CC=${CC}"
-echo "CXX=${CXX}"
-echo "CC_FOR_BUILD=${CC_FOR_BUILD}"
-echo "CXX_FOR_BUILD=${CXX_FOR_BUILD}"
-echo "CFLAGS_FOR_BUILD=${CFLAGS_FOR_BUILD}"
-echo "CXXFLAGS_FOR_BUILD=${CXXFLAGS_FOR_BUILD}"
-echo "LDFLAGS_FOR_BUILD=${LDFLAGS_FOR_BUILD}"
-echo "AR_FOR_BUILD=${AR_FOR_BUILD}"
-echo "RANLIB_FOR_BUILD=${RANLIB_FOR_BUILD}"
-
 export CXXFLAGS="$CXXFLAGS -D_LIBCPP_DISABLE_AVAILABILITY"
-
-case "$target_platform" in
-osx-*)
-	with_others="
-		--with-opengl=osx
-		--with-x=no
-	"
-	;;
-esac
 
 if [ "$CONDA_BUILD_CROSS_COMPILATION" = "1" ]; then
 	(
@@ -35,7 +9,6 @@ if [ "$CONDA_BUILD_CROSS_COMPILATION" = "1" ]; then
 	export CXX="${CXX_FOR_BUILD}"
 	export AR="${AR_FOR_BUILD}"
 	export RANLIB="${RANLIB_FOR_BUILD}"
-
 	export CPPFLAGS="-I${BUILD_PREFIX}/include ${CPPFLAGS}"
 	export LDFLAGS="-L${BUILD_PREFIX}/lib ${LDFLAGS}"
 
@@ -56,7 +29,14 @@ if [ "$CONDA_BUILD_CROSS_COMPILATION" = "1" ]; then
 			exit 1
 		)
 
-	for tool in include lib/datetime lib/gis utils general/g.parser; do
+	for tool in \
+		include \
+		lib/datetime \
+		lib/gis \
+		utils \
+		general/g.parser \
+		general/g.mkfontcap \
+	; do
 		make -j$CPU_COUNT -C $tool
 	done
 	)
@@ -64,6 +44,15 @@ if [ "$CONDA_BUILD_CROSS_COMPILATION" = "1" ]; then
 	dist_build="$(pwd)/dist.$BUILD"
 	sed -Ei 's#(\tPATH=")#\1'"$dist_build"'/bin:#' include/Make/Rules.make
 fi
+
+case "$target_platform" in
+osx-*)
+	with_others="
+		--with-opengl=osx
+		--with-x=no
+	"
+	;;
+esac
 
 ./configure \
 	--prefix=$PREFIX \
